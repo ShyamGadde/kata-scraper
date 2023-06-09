adc576ef9a6e66d9b7967a468f008680a050e011
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-06-10 03:59:25 +0530

    Add type hints

--- utils/auth.py
+++ utils/auth.py
@@ -2,21 +2,22 @@ import sys
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
-async def sign_in_to_codewars(driver, email, codewars_password):
+async def sign_in_to_codewars(
+    driver: WebDriver, codewars_email: str, codewars_password: str
+):
     try:
         driver.get("https://www.codewars.com/users/sign_in")
     except TimeoutError:
         print("The driver took too much time.")
         sys.exit(1)
 
-    email = "shyamgadde1602@gmail.com"
-    codewars_password = "Q!2gxNAM^%M!U&"
-
     try:
-        sign_in_form = driver.find_element(By.ID, "new_user")
-        driver.find_element(By.ID, "user_email").send_keys(email)
+        sign_in_form: WebElement = driver.find_element(By.ID, "new_user")
+        driver.find_element(By.ID, "user_email").send_keys(codewars_email)
         driver.find_element(By.ID, "user_password").send_keys(codewars_password)
         sign_in_form.find_element(By.CLASS_NAME, "is-red").click()
     except NoSuchElementException:
