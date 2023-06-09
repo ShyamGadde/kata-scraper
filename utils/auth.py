1cd43637f604dbd23faef6919aad4b63f81b8d7b
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-06-10 03:53:00 +0530

    fix: exception handling for sign in

--- utils/auth.py
+++ utils/auth.py
@@ -1,14 +1,24 @@
+import sys
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
 async def sign_in_to_codewars(driver, email, codewars_password):
     try:
         driver.get("https://www.codewars.com/users/sign_in")
-    except:
+    except TimeoutError:
         print("The driver took too much time.")
-        return
+        sys.exit(1)
+
+    email = "shyamgadde1602@gmail.com"
+    codewars_password = "Q!2gxNAM^%M!U&"
 
     try:
         sign_in_form = driver.find_element(By.ID, "new_user")
         driver.find_element(By.ID, "user_email").send_keys(email)
         driver.find_element(By.ID, "user_password").send_keys(codewars_password)
         sign_in_form.find_element(By.CLASS_NAME, "is-red").click()
-    except:
+    except NoSuchElementException:
         print("A web element was not found on the page (sign-in step).")
+        sys.exit(1)
